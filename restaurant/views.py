439f5fdb4e297cd6a571324ae33aadf9a9e98d93
Đức Nguyễn Mạnh <102190058@sv1.dut.udn.vn>
2022-06-20 16:54:08 +0700

    UpdateProject

--- restaurant/views.py
+++ restaurant/views.py
@@ -14,17 +14,15 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
             created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
 
         # loop through the orders and add the price value, check if order is not shipped
-        unshipped_orders = []
+        _orders = []
         total_revenue = 0
         for order in orders:
             total_revenue += order.price
-
-            if not order.is_shipped:
-                unshipped_orders.append(order)
+            _orders.append(order)
 
         # pass total number of orders and total revenue into template
         context = {
-            'orders': unshipped_orders,
+            'orders': _orders,
             'total_revenue': total_revenue,
             'total_orders': len(orders)
         }
@@ -46,7 +44,6 @@ class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
 
     def post(self, request, pk, *args, **kwargs):
         order = OrderModel.objects.get(pk=pk)
-        order.is_shipped = True
         order.save()
 
         context = {
